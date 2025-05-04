38386e2c5c81d8d7af90cf1f4aa1b91b1874dd64
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-05-04 17:39:23 +0000

    fixing synatax

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -1,16 +1,24 @@
 public class Card { // https://www.geeksforgeeks.org/private-and-final-methods-in-java/
-2    private final String name; //private means that the variable can only assessed from the class it is decleared in
-3    private final String suit; // final means it can only be assigned once
-4    private final int value; // using both makes them "immutable". lets say I want my card "banana" to stay "banana" for all time. this is useful
-//card
+  private final String name; // private means that the variable can only be accessed from the class it is declared in
+  private final String suit; // final means it can only be assigned once
+  private final int value;   // using both makes them "immutable". let's say I want my card "banana" to stay "banana" for all time. this is useful
+
+  // card
   public Card(String name, String suit, int value) {
-    this.name = name; //stores the value inside the variable
-    this.suit = suit: //does the exact same thing again
-    this.value = value: // AGAIN!!!.
+    this.name = name;  // stores the value inside the variable
+    this.suit = suit;  // does the exact same thing again
+    this.value = value; // AGAIN!!!
   }
 
-  public String getName () { //returns my values
+  public String getName() { // returns my values
     return name;
   }
 
-  
+  public String getSuit() { 
+    return suit;
+  }
+
+  public int getValue() {
+    return value;
+  }
+}
