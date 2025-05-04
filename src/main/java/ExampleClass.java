305cc4cc83f7d8665a209cc207965fcc076e86b2
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-05-04 17:36:34 +0000

    day 1

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -1,4 +1,16 @@
+public class Card { // https://www.geeksforgeeks.org/private-and-final-methods-in-java/
+2    private final String name; //private means that the variable can only assessed from the class it is decleared in
+3    private final String suit; // final means it can only be assigned once
+4    private final int value; // using both makes them "immutable". lets say I want my card "banana" to stay "banana" for all time. this is useful
+//card
+  public Card(String name, String suit, int value) {
+    this.name = name; //stores the value inside the variable
+    this.suit = suit: //does the exact same thing again
+    this.value = value: // AGAIN!!!.
+  }
 
-class ExampleClass {
-	
-}
+  public String getName () { //returns my values
+    return name;
+  }
+
+  
