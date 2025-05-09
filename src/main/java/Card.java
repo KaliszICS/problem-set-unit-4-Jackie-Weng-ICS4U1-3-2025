85b3e7eb7430967846a875b0e5bf225008be4918
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-05-09 03:39:44 +0000

    changing variable names

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,31 +1,31 @@
 
 final class Card { 
   private  String name; // private means that the variable can only be accessed from the class it is declared in
-  private  String types; // final means it can only be assigned once
+  private  String suits; // final means it can only be assigned once
   private int value;   // using both makes them "immutable". let's say I want my card "banana" to stay "banana" for all time. this is useful
 
   
-  public Card(String name, String types, int value) {
+  public Card(String name, String suits, int value) {
     this.name = name;  // stores the value inside the variable
-    this.types = types;  // does the exact same thing again, 
-    this.value = value; // AGAIN!!!
+    this.suits = suits;  // does the exact same thing again, 
+    this.value = value; // again
   }
 
   public String getName() { // returns my values
     return name;
   }
 
-  public String getTypes() { // AGAIN!!!
-    return types;
+  public String getSuits() { // again
+    return suits;
   }
 
-  public int getValue() { //AGAIN!!! 
+  public int getValue() { //again
     return value;
   }
 
 @Override
-public String toString() { // string to string combine my names and types. creates something like "Ace of Hearts" or "King of Spades"
-    return name + " of " + types; // all cards no matter what have a "of" in the middle. 
+public String toString() { // string to string combine my names and suits. creates something like "Ace of Hearts" or "King of Spades"
+    return name + " of " + suits; // all cards no matter what have a "of" in the middle. 
   }
 @Override
 public boolean equals(Object obj) {
@@ -38,12 +38,12 @@ public boolean equals(Object obj) {
   } 
 
   Card card = (Card) obj;
-  return value == card.value && name.equals(card.name) && types.equals(card.types); // checks if any 2 cards are equal, the value of the card depends on 3 things.
+  return value == card.value && name.equals(card.name) && suits.equals(card.suits); // checks if any 2 cards are equal, the value of the card depends on 3 things.
 } 
   @Override
   public int hashCode() { //https://kiranukamath.medium.com/understanding-hashcode-and-equals-in-java-9041b1c418ef
     int result = name.hashCode();
-    result = 7 * result + types.hashCode();  // you can use any PRIME NUMBER. I just like the number 7. its the day my dog was born
+    result = 7 * result + suits.hashCode();  // you can use any PRIME NUMBER. I just like the number 7. its the day my dog was born
     result = 7 * result + value; //
     return result;
   }
