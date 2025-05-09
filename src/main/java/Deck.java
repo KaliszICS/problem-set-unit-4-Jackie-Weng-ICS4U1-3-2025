b8fba8c20e5c45d00f2f04815a578cbfd4f86539
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-05-09 01:54:14 +0000

    comments

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,11 +1,11 @@
 //import
 class Deck {
-  private Card[] cards;
-  private int cardslengths;
+  private Card[] cards; //simple array. our "cards"
+  private int cardslengths; // from unit one. tracks our cardlength or how many are currently in the deck.
 
 
   public Deck(Card[] cardsArray) {
-    this.cardslengths = cardsArray.length;x
+    this.cardslengths = cardsArray.length;
     this.cards = new Card[cardslengths];
     for (int i = 0; i < cardslengths; i++) {
       this.cards[i] = cardsArray[i];
@@ -13,10 +13,10 @@ class Deck {
   }
 
 public Deck() {
-  String[] suits = {"H", "C", "D", "S"};
-  String[] names = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-  this.cards = new Card[52];
-  this.cardslengths = 0;
+  String[] suits = {"H", "C", "D", "S"}; // simple array.
+  String[] names = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}; //simple array.
+  this.cards = new Card[52]; // 52 gives us just enough space for all cards. 4 suits 13 cards gives us just enough of 52 cards.
+  this.cardslengths = 0; 
 
 
   for (int sx = 0; sx < suits.length; sx++) {
@@ -31,15 +31,25 @@ public int cardslengths() {
 }
 
 public Card drawingcards() {
-  if (cardslengths == 0) {
+  if (cardslengths == 0) { // if you have zero cards, you arent playing anymore. this checks if you do and returns null.
     return null;
   }
  
 Card topCard = cards[0];
-for (int ysd = 1; ysd < size; ysd++) {
+for (int ysd = 1; ysd < cardslengths; ysd++) {
   cards[ysd - 1] = cards[ysd]; 
   }
+  cards[--cardslengths] = null;
+  return topCard;
 }
 
-public void shufflecards() {
-  for (int ysd = size)
+
+public void shufflecards() { //you could prob simpify this by using the shuffle method in java already. but you need a import
+  for (int ysd = cardslengths - 1; ysd > 0; ysd--) { // from unit one. starts the last index of the array and move backwards
+    int dsy = (int)(Math.random() * (ysd + 1)); // also from unit 1. math.random returns a double value
+    Card temp = cards[ysd]; // temp stores the card and index ysd
+    cards[ysd] = cards[dsy]; // randomly swaps them.
+    cards [dsy] = temp;
+  }
+}
+} 
