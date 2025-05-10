32bb190decb65b2a954637d5a8b5433060547a12
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-05-10 01:43:34 +0000

    formatting

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -4,13 +4,15 @@
 
 final class Card { 
   private  final String name; // private means that the variable can only be accessed from the class it is declared in
+  
   private final String suits; // final means it can only be assigned once
 
 
   private final int value;   // each card has their own values, a ace is worth 1 which is like nothing while a king is worth 13
 /* 
- * @param Name, the name of the card. 
- * @param Suit, the suit/categories of the card
+ * Creating a card with 3 thing
+ * * @param Suit, the suit/categories of the card. Something like a Heart or a spade
+ * @param Name, the name of the card. something like a King or a Ace
  * @param Value, the hierarchy of the card/how much the card is worth compared to others
  */
   
@@ -29,14 +31,14 @@ final class Card {
 /* @return suit as a string so its actually readable */
 
   public String getSuits() {  
-    return suits;
+    return this.suits;
   }
 
 
 
 /* @return return the name of the card also as a string */
   public String getName() { // returns my values
-    return name; // you can either return name or this.name. As long as you stay consistent it doesn't really matter
+    return this.name; // you can either return name or this.name. As long as you stay consistent it doesn't really matter
   }
 
 
@@ -44,7 +46,7 @@ final class Card {
   /* @return return value of the card */
 
   public int getValue() { 
-    return value;
+    return this.value;
   }
 
 
@@ -54,9 +56,9 @@ final class Card {
 
 @Override
 public String toString() { //  creates something like "Ace of Hearts" or "King of Spades"
-    return name + " of " + suits; // all cards no matter what have a "of" in the middle. 
+    return this.name + " of " + this.suits; // all cards no matter what have a "of" in the middle. 
   }
-  /* @param obj, something to compare each card with */
+  /* @param obj, something to compare each card with, Not necessary another card */
 
 
 
@@ -65,12 +67,14 @@ public boolean equals(Object obj) {
   if (obj == null || getClass() != obj.getClass()) { // checks if its null. if it is it just returns false 
     return false; // instead of crashing because of the null it returns false
  
+    /* @return true if the 2 cards have the same suit, value and name */
   } 
   //checking memory just in case
   if (this == obj) { // it just checks if the instance is the same as the object being compared
     return true; 
   } 
 
+/* Checking if cards are the same from their 3 things */
 
   Card card = (Card) obj; // first casting the obj to a card. you cant compare cards if one isnt a card.
   return value == card.value && name.equals(card.name) && suits.equals(card.suits); // checks all 3 fields
--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,9 +1,9 @@
-//import java.util.ArrayList; 
+//import java.util.ArrayList; not needed as im not merging any 2 decks together. regular arrays are just fine.
 
 /* my deck class is just a normal standard deck of fully virtual cards */
 /* shuffles the cards without imports with math.random */
 /* Size: the # of cards in the deck.  */
-
+/* Also immutable, i cant modify anything */
 class Deck { 
   private Card[] cards; 
   private int size;  // size is just  how many cards I have
@@ -12,20 +12,20 @@ class Deck {
 //* each card has a suit, a name and a value! */
  
 //creating a new array with methods from unit 1
-
+//* 53 total amount of cards in a deck */
 public Deck() {
   String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"}; // simple array 
   String[] names = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}; //simple array.
   int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // the 13 values, Ace through king
   
-  // this.cards = new Card[52]; // too Hard coded
+  // this.cards = new Card[52]; // too Hard coded, 
   
-  cards = new Card[suits.length * names.length]; // you could hardcode it like the comment above, but you could also just mulitply them to get >=53
+  cards = new Card[suits.length * names.length]; // you could hardcode it like the comment above, but you could also just mulitply them to get 52
   size = 0; // starts at zero cards 
 
  
   
-//looping
+/* looping */
   for (int x = 0; x < suits.length; x++) { // loops though each and every suit 
     for (int y = 0; y < names.length; y++)  {  
       int value = values[y];
@@ -36,7 +36,7 @@ public Deck() {
 
 
 /* @param Cardsarray, an array of cards */
-
+/* create a "deck" from a array of cards */
 
   public Deck(Card[] cardsArray) {
     size = cardsArray.length; // from unit one. this simply just sets the number of cards of size of array.
@@ -66,7 +66,7 @@ public Card draw() {
 
 
 
-//*finding top card using a stack approach simiarly to the tower of hanoi *//
+//*finding top card using a stack approach (familiar to the tower of hanoi) *//
 Card topCard = cards[--size]; // decreases size and get the card at the new index, which is the "top" card
  cards[size] = null; // you dont have to do this, but if you set the postion to null it saves a bit on memory
  return topCard; // return the top card that was drawn. 
